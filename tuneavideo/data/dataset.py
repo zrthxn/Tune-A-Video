db0698a1e41786edf1914e3467bfa7f8a4fbb62b
Alisamar Husain <zrthxn@gmail.com>
2024-04-25 15:58:54 +0200

    CV2

--- tuneavideo/data/dataset.py
+++ tuneavideo/data/dataset.py
@@ -1,8 +1,53 @@
-import decord
-decord.bridge.set_bridge('torch')
-
+import os
+import cv2
+import torch
 from torch.utils.data import Dataset
 from einops import rearrange
+
+
+def extract_frames(video_path: str, start = -1, end = -1) -> torch.Tensor:
+    """Extract frames from a video using OpenCVs VideoCapture
+
+    Args:
+        video_path (str): path of the video
+        start (int, optional): start frame. Defaults to -1.
+        end (int, optional): end frame. Defaults to -1.
+
+    Returns:
+        torch.Tensor: Frames in a Tensor
+    """
+
+    video_path = os.path.normpath(video_path)  # make the paths OS (Windows) compatible
+    assert os.path.exists(video_path)  # assert the video file exists
+
+    capture = cv2.VideoCapture(video_path)  # open the video using OpenCV
+    if start < 0:  # if start isn't specified lets assume 0
+        start = 0
+    if end < 0:  # if end isn't specified assume the end of the video
+        end = int(capture.get(cv2.CAP_PROP_FRAME_COUNT))
+
+    capture.set(1, start)  # set the starting frame of the capture
+    frame = start  # keep track of which frame we are up to, starting from start
+    while_safety = 0  # a safety counter to ensure we don't enter an infinite while loop (hopefully we won't need it)
+
+    frames = list()
+    while frame < end:  # lets loop through the frames until the end
+
+        _, image = capture.read()  # read an image from the capture
+
+        if while_safety > 500:  # break the while if our safety maxs out at 500
+            break
+
+        # sometimes OpenCV reads None's during a video, in which case we want to just skip
+        if image is None:  # if we get a bad return flag or the image we read is None, lets not save
+            while_safety += 1  # add 1 to our while safety, since we skip before incrementing our frame variable
+            continue  # skip
+        
+        frames.append(image)
+        frame += 1  # increment our frame count
+
+    capture.release()  # after the while has finished close the capture
+    return torch.cat(frames)
 
 
 class TuneAVideoDataset(Dataset):
@@ -31,9 +76,10 @@ class TuneAVideoDataset(Dataset):
 
     def __getitem__(self, index):
         # load and sample video frames
-        vr = decord.VideoReader(self.video_path, width=self.width, height=self.height)
-        sample_index = list(range(self.sample_start_idx, len(vr), self.sample_frame_rate))[:self.n_sample_frames]
-        video = vr.get_batch(sample_index)
+        # vr = decord.VideoReader(self.video_path, width=self.width, height=self.height)
+        # sample_index = list(range(self.sample_start_idx, len(vr), self.sample_frame_rate))[:self.n_sample_frames]
+        # video = vr.get_batch(sample_index)
+        video = extract_frames(self.video_path, self.sample_start_idx, self.sample_start_idx + self.n_sample_frames)
         video = rearrange(video, "f h w c -> f c h w")
 
         example = {
